e579ec19dccee0323618d585c072d11c9d5d76ea
David <david@rlabs.org>
2024-12-01 23:47:34 +0200

    remove : nuxt boiler app compoents

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="w-full text-center">
+    <!--handle layouts-->
+    <h1 class="text-3xl font-bold">Blogger.</h1>
   </div>
 </template>
